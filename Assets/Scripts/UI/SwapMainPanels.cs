6e0d02c3efb60a5a256e3317e72474c425d29c4d
SyruSTR <syrus_tr@mail.ru>
2020-03-27 22:50:07 +0800

    first commit

---
+++ Assets/Scripts/UI/SwapMainPanels.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SwapMainPanels : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    // Start is called before the first frame update
+    private Vector2 startPos;
+    [SerializeField] private float lerpSpeed = 1f;
+    private bool directionChosen;    
+    [SerializeField] float choosenScreenX = 0f;
+    [SerializeField] float deltaX = 5.6f;
+    [SerializeField] private Text textX;
+    [SerializeField] private Text textXScreen;
+    private int activeScreen;
+    private Vector2 touchPos;
+    private bool freezeSwap;
+    public bool FreezeSwap { set { freezeSwap = value; } }
+    void Start()
+    {
+        activeScreen = 2;
+        directionChosen = true;
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            transform.position = new Vector3(0, 0, 0);
+        }
+        if (directionChosen)
+        {
+            ScreenChosen();
+        }
+        textX.text = "X: " + transform.position.x;
+        textXScreen.text = "ActiveScr: " + choosenScreenX.ToString();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //startPos = eventData.delta;
+        directionChosen = false;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+
+        if (!freezeSwap)
+        {
+            touchPos = eventData.delta;
+            if (Mathf.Abs(touchPos.x) > Mathf.Abs(touchPos.y))
+            {
+                var targetPos = Mathf.Clamp(transform.position.x, choosenScreenX - 3f, choosenScreenX + 3f);
+                transform.position = new Vector3(Mathf.Lerp(targetPos, transform.position.x + touchPos.x / 5, lerpSpeed * Time.deltaTime), transform.position.y, transform.position.z);
+            }
+        }
+        else
+            directionChosen = true;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (!freezeSwap)
+        {
+            if (Mathf.Abs(touchPos.x) > Mathf.Abs(touchPos.y))
+            {
+                ActivateDeactivateSwapUpDown(false);
+                if ((choosenScreenX - transform.position.x) > 1f && activeScreen < 4)
+                {
+                    choosenScreenX -= deltaX;
+                    activeScreen++;
+                }
+                else if ((choosenScreenX - transform.position.x) < 1f && activeScreen > 0)
+                {
+                    choosenScreenX += deltaX;
+                    activeScreen--;
+                }
+                ActivateDeactivateSwapUpDown(true);
+            }
+        }
+        directionChosen = true;
+    }
+    private void ActivateDeactivateSwapUpDown(bool enabled)
+    {
+        SwapUpDown activeScreenSwapping = transform.GetChild(activeScreen).GetComponent<SwapUpDown>();
+        if (activeScreenSwapping != null)
+        {
+            StopCoroutine(activeScreenSwapping.BackY());
+            if (!enabled)
+            {
+                StartCoroutine(activeScreenSwapping.BackY());
+            }
+            activeScreenSwapping.enabled = enabled;
+        }
+    }
+    private void ScreenChosen()
+    {
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, choosenScreenX, lerpSpeed * 3 * Time.deltaTime), transform.position.y, transform.position.z);
+    }
+    public void ButtonChosenScreen(float screenNumber)
+    {
+        if (activeScreen != screenNumber && screenNumber >= 0 && screenNumber <= 4)
+        {
+            ActivateDeactivateSwapUpDown(false);
+            choosenScreenX = screenNumber;
+            freezeSwap = false;
+            choosenScreenX = screenNumber;
+            ActivateDeactivateSwapUpDown(true);
+        }
+    }
+
+}
---
+++ Assets/Scripts/UI/SwapUpDown.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SwapUpDown : MonoBehaviour
+{
+    [SerializeField] private float lerpSpeed = 3f;
+    [SerializeField] private Camera cam;
+    [SerializeField] private float coefficientSwapSpeed = 5f;
+    [SerializeField] private float minBorder;
+    [SerializeField] private float maxBorder;
+    [Space]
+    [SerializeField] private Text textY;
+    
+    private Vector3 touchPos;
+    private Vector3 lastTouchPos;
+    private bool swappingABorderB;
+    private float swappingVar;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        swappingABorderB = false;
+    }
+    public IEnumerator BackY()
+    {
+        yield return new WaitForSeconds(0.3f);
+        transform.position = new Vector3(transform.position.x, minBorder, transform.position.z);
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.G))
+        {
+            transform.position = new Vector3(0, 0, 0);
+        }
+        textY.text = "Y: " + transform.position.y;
+        
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            var transformY = transform.position.y;
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    lastTouchPos = cam.ScreenToWorldPoint(touch.position);
+                    break;
+                case TouchPhase.Moved:
+                    touchPos = coefficientSwapSpeed * (cam.ScreenToWorldPoint(touch.position) - lastTouchPos);
+
+                    if (Mathf.Abs(touchPos.x) < Mathf.Abs(touchPos.y))
+                        GetComponentInParent<SwapMainPanels>().FreezeSwap = true;
+                    if (Mathf.Abs(touchPos.y) > Mathf.Abs(touchPos.x) && touch.position.y > 200)
+                    {
+                        var targetPos = Mathf.Clamp(transformY, minBorder - 3f, maxBorder + 3f);
+                        transform.position = new Vector3(transform.position.x, Mathf.Lerp(targetPos, transformY + touchPos.y, lerpSpeed * Time.deltaTime), transform.position.z);
+
+                    }
+                    lastTouchPos = cam.ScreenToWorldPoint(touch.position);
+                    break;
+                case TouchPhase.Ended:
+                    
+                    
+                        StartCoroutine(LerpEnd());
+                    GetComponentInParent<SwapMainPanels>().FreezeSwap = false;
+                    lastTouchPos = Vector3.zero;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (swappingABorderB)
+        {
+            
+            SwappingABorder(swappingVar);
+        }
+    }
+    IEnumerator LerpEnd()
+    {
+        var stopingSpeed = 2f;
+        if (Mathf.Abs(touchPos.y) > 1f)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                if (transform.position.y > maxBorder || transform.position.y < minBorder)
+                    break;
+                var targetPos = Mathf.Clamp(transform.position.y, minBorder, maxBorder);
+                transform.position = new Vector3(transform.position.x, Mathf.Lerp(targetPos, transform.position.y + touchPos.y * 3f, Time.deltaTime * lerpSpeed * 1.5f / stopingSpeed), transform.position.z);
+                stopingSpeed += 0.7f;
+                yield return new WaitForSeconds(0.01f);
+            }
+        }
+        if (transform.position.y > (maxBorder + 0.1f))
+        {
+
+            swappingVar = maxBorder;
+            swappingABorderB = true;
+
+        }
+        if (transform.position.y < (minBorder - 0.1f))
+        {
+
+            swappingVar = minBorder;
+            swappingABorderB = true;
+        }
+
+
+    }
+    private void SwappingABorder(float variable)
+    {
+        if (transform.position.y < minBorder || transform.position.y > maxBorder)
+        {
+
+            transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, variable, lerpSpeed * 2.5f * Time.deltaTime), transform.position.z);
+        }
+        else
+            swappingABorderB = false;
+    }
+}
